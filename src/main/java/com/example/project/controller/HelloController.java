dd3503b8b96864ffb6037172d9108b070a84d24b
tjxor <seotaegeon@gmail.com>
2022-12-23 02:19:00 +0900

    CI/CD

--- src/main/java/com/example/project/controller/HelloController.java
+++ src/main/java/com/example/project/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 //    http://localhost:8080/swagger-ui/index.html
     @GetMapping("/api/v1/hello")
     public String hello() {
-        return "ccccccccccccccccczzz";
+        return "czxczxczxc";
     }
 
 }
